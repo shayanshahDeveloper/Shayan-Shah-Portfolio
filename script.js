305047b444203cedd20b8f2b2c83ec9b51cadac6
moviesworldbyshanie <127875416+moviesworldbyshanie@users.noreply.github.com>
2024-08-15 01:01:19 +0500

    Assets Changes & Minor Bugs fix

--- script.js
+++ script.js
@@ -3,4 +3,32 @@ const container = document.querySelector(".container");
 const cloneContainer = container.cloneNode(true);
 cloneContainer.id = "dark-container";
 
-console.log9clo;
+document.body.appendChild(cloneContainer);
+cloneContainer.classList.remove("active");
+
+const toggleIcons = document.querySelectorAll(".toggle-icon");
+const icons = document.querySelectorAll(".toggle-icon i");
+const darkContainer = document.querySelector("#dark-container");
+
+// Dark Mode//
+
+const darkContainerImg = document.querySelector(
+  "#dark-container .home-img img"
+);
+darkContainerImg.src = "./assets/darkmode.png";
+
+toggleIcons.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.classList.add("disabled");
+
+    setTimeout(() => {
+      toggle.classList.remove("disabled");
+    }, 1500);
+
+    icons.forEach((icon) => {
+      icon.classList.toggle("bx-sun");
+    });
+    container.classList.toggle("active");
+    darkContainer.classList.toggle("active");
+  });
+});
